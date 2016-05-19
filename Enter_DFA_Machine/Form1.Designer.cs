9cb15621018f67b952ed0fff22379b54e9fd9ea4
Abn Najaf <abn.najaf@gmail.com>
2016-05-19 21:35:19 +0430

    First Commit

---
+++ Enter_DFA_Machine/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Enter_DFA_Machine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.Gv1 = new System.Windows.Forms.DataGridView();
+            this.ColQ = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColA = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColB = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColFinal = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.BtnAdd = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtString = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Gv1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Gv1
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Gv1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.Gv1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.Gv1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.Gv1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Gv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Gv1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColQ,
+            this.ColA,
+            this.ColB,
+            this.ColFinal});
+            this.Gv1.GridColor = System.Drawing.Color.WhiteSmoke;
+            this.Gv1.Location = new System.Drawing.Point(1, 1);
+            this.Gv1.Name = "Gv1";
+            this.Gv1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.Gv1.Size = new System.Drawing.Size(436, 371);
+            this.Gv1.TabIndex = 0;
+            this.Gv1.RowValidated += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_RowLeave);
+            // 
+            // ColQ
+            // 
+            this.ColQ.Frozen = true;
+            this.ColQ.HeaderText = "Q";
+            this.ColQ.Name = "ColQ";
+            // 
+            // ColA
+            // 
+            this.ColA.HeaderText = "a";
+            this.ColA.Name = "ColA";
+            // 
+            // ColB
+            // 
+            this.ColB.HeaderText = "b";
+            this.ColB.Name = "ColB";
+            // 
+            // ColFinal
+            // 
+            this.ColFinal.HeaderText = "F";
+            this.ColFinal.Name = "ColFinal";
+            // 
+            // BtnAdd
+            // 
+            this.BtnAdd.Location = new System.Drawing.Point(517, 149);
+            this.BtnAdd.Name = "BtnAdd";
+            this.BtnAdd.Size = new System.Drawing.Size(156, 66);
+            this.BtnAdd.TabIndex = 1;
+            this.BtnAdd.Text = "Read String";
+            this.BtnAdd.UseVisualStyleBackColor = true;
+            this.BtnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(517, 221);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(156, 67);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "Reset ";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Location = new System.Drawing.Point(517, 294);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(156, 67);
+            this.btnHelp.TabIndex = 3;
+            this.btnHelp.Text = "Help";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(475, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "String :";
+            // 
+            // txtString
+            // 
+            this.txtString.Location = new System.Drawing.Point(478, 33);
+            this.txtString.Name = "txtString";
+            this.txtString.Size = new System.Drawing.Size(195, 20);
+            this.txtString.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 373);
+            this.Controls.Add(this.txtString);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.BtnAdd);
+            this.Controls.Add(this.Gv1);
+            this.Name = "Form1";
+            this.Text = "Enter DFA";
+            ((System.ComponentModel.ISupportInitialize)(this.Gv1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView Gv1;
+        private System.Windows.Forms.Button BtnAdd;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtString;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColQ;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColA;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColB;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn ColFinal;
+    }
+}
+
---
+++ Enter_DFA_Machine/Form1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Enter_DFA_Machine
+{
+    public partial class Form1 : Form
+    {
+        public bool IsNotDFA { get; private set; }
+
+        //ABN.NAJAF@GMAIL.COM
+        //ابوالفضل نجف زاده
+
+        //ماشین DFA
+        //الفبا =={a,b}
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void dataGridView1_RowLeave(object sender, DataGridViewCellEventArgs e)
+        {
+            ///سه شرط داشتیم برای 
+            ///DFA
+            /// بودن که عبارتند از :
+            /// 1- از هر وضعیت فقط یک حرف از الفبا خارج شود
+            /// 2- تمام حروف الفبا برای هر وضعیت باید مشخص شوند
+            /// 3- یک انتقال نمیتواند لاندا باشد
+
+            /// که این سه شرط را در پایین بررسی میکنیم
+
+
+            try
+            {
+                if (Gv1.Rows[e.RowIndex].Cells[0].Value == null &&
+                    Gv1.Rows[e.RowIndex].Cells[1].Value == null &&
+                    Gv1.Rows[e.RowIndex].Cells[2].Value == null)
+                {
+                    Gv1.Rows[e.RowIndex].ErrorText = "";
+                    IsNotDFA = false;
+                    return;
+                }
+
+                ////////////////شرط اول و دوم //////////////////////
+                ///این قسمت چک میکند که مقادیر ورودی فقط عدد باشند
+                /// و نمیتواند برای یکی از وضعیت های لاندا قرار گیرد
+                int numQ = Int32.Parse(Gv1.Rows[e.RowIndex].Cells[0].Value.ToString());
+                int numA = Int32.Parse(Gv1.Rows[e.RowIndex].Cells[1].Value.ToString());
+                int numB = Int32.Parse(Gv1.Rows[e.RowIndex].Cells[2].Value.ToString());
+                ////////////////////////////////////
+                Gv1.Rows[e.RowIndex].ErrorText = "";
+                IsNotDFA = false;
+
+
+
+
+                ////////////////شرط سوم //////////////////////
+                ///که چک میکند هر یک از وضعیت ها فقط یک بار تکرار شده باشند
+                /// وضعیت ها == SATE
+                for (int i = 0; i < Gv1.Rows.Count - 1; i++)
+                {
+                    if (Gv1.Rows[e.RowIndex].Cells[0].Value.Equals(Gv1.Rows[i].Cells[0].Value) &&
+                        !Gv1.Rows[e.RowIndex].Index.Equals(Gv1.Rows[i].Index))
+                    {
+                        Gv1.Rows[e.RowIndex].ErrorText = "وضعیت وارد شده تکراری است";
+                        IsNotDFA = true;
+                        continue;
+                    }
+                }
+            }
+            catch (Exception ee)
+            {
+                Gv1.Rows[e.RowIndex].ErrorText = "داده های ورودی صحیح نیست";
+                IsNotDFA = true;
+            }
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Gv1.RowCount <= 1)
+                {
+                    MessageBox.Show("Is Not DFA! ! No Data", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                //Sorted
+                DataGridViewColumn gvc = Gv1.Columns[0] as DataGridViewColumn;
+                Gv1.Sort(gvc, ListSortDirection.Ascending);
+                //End Sorted
+
+
+
+
+                //این حالتی را بررسی میکند که از یک وضعیت به یک وضعیت دیگه ای رفتیم که وضعیت مقصد برایش 
+                //a , b
+                // مشخص نشده است.
+                string StateA, StateB;
+                bool IsExistA = false;
+                bool IsExistB = false;
+                for (int i = 0; i < Gv1.RowCount - 1; i++)
+                {
+                    StateA = Gv1.Rows[i].Cells[1].Value.ToString();
+                    StateB = Gv1.Rows[i].Cells[2].Value.ToString();
+
+                    for (int j = 0; j < Gv1.RowCount - 1; j++)
+                    {
+                        if (Gv1.Rows[j].Cells[0].Value.Equals(StateA))
+                        {
+                            IsExistA = true;
+                        }
+                        if (Gv1.Rows[j].Cells[0].Value.Equals(StateB))
+                        {
+                            IsExistB = true;
+                        }
+                    }
+                    if (!IsExistA)
+                    {
+                        Gv1.Rows[i].Cells[1].ErrorText = "Not Find in State";
+                    }
+                    else
+                    {
+                        Gv1.Rows[i].Cells[1].ErrorText = "";
+                    }
+
+
+                    if (!IsExistB)
+                    {
+                        Gv1.Rows[i].Cells[2].ErrorText = "Not Find in State";
+                    }
+                    else
+                    {
+                        Gv1.Rows[i].Cells[2].ErrorText = "";
+                    }
+                    IsExistA = IsExistB = false;
+                }
+
+
+
+
+
+
+                //این حلقه اگر قبلا خطایی اعلام شده بود که رفع نشده باشد را مشخص میکند
+                foreach (DataGridViewRow item in Gv1.Rows)
+                {
+                    if (!item.ErrorText.Equals("") ||
+                        !item.Cells[1].ErrorText.Equals("") ||
+                        !item.Cells[2].ErrorText.Equals(""))
+                    {
+                        MessageBox.Show("Not DFA!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                }
+
+
+
+
+
+                if (ReadString())
+                {
+                    MessageBox.Show("Accepted", "Accept", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Rejected", "Reject", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show("Not DFA!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private Boolean ReadString()
+        {
+            try
+            {
+                String Str = txtString.Text.Trim();
+                //در این حلقه ما چک میکنیم فقط حروف الفبا ما وارد شده باشد
+                //الفبا a , b
+                foreach (Char item in Str.ToCharArray())
+                {
+                    if (!(item.Equals('a') || item.Equals('b')))
+                    {
+                        MessageBox.Show("حروف ورودی عضو الفیای ما نمی باشند", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return false;
+                    }
+                }
+
+
+
+
+
+                //یافتن حالت های پایانی
+                List<String> FinalState = new List<string>();
+                for (int i = 0; i < Gv1.RowCount - 1; i++)
+                {
+                    if (Gv1.Rows[i].Cells[3].Value != null && Gv1.Rows[i].Cells[3].Value.Equals(true))
+                    {
+                        FinalState.Add(Gv1.Rows[i].Cells[0].Value.ToString());
+                    }
+                }
+
+
+
+
+
+
+                String currentState = Gv1.Rows[0].Cells[0].Value.ToString();
+
+                foreach (Char item in Str.ToCharArray())
+                {
+                    //این حلقه برای این است که اندیس سطر حالت فعلی را بدست آوریم
+                    int IndexCurrentRow = 0;
+                    foreach (DataGridViewRow GvrItem in Gv1.Rows)
+                    {
+                        if (GvrItem.Cells[0].Value.Equals(currentState))
+                        {
+                            IndexCurrentRow = GvrItem.Index;
+                            break;
+                        }
+                    }
+
+
+                    switch (item)
+                    {
+                        case 'a':
+                            {
+                                currentState = Gv1.Rows[IndexCurrentRow].Cells[1].Value.ToString();
+                            }; break;
+                        case 'b':
+                            {
+                                currentState = Gv1.Rows[IndexCurrentRow].Cells[2].Value.ToString();
+
+                            }; break;
+                    }
+                }
+
+
+                //بررسی این که آیا با خواندن رشته در حالت پایانی متوقف شده ایم 
+                if (FinalState.IndexOf(currentState) >= 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            catch (Exception ee)
+            {
+                return false;
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            Gv1.Rows.Clear();
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start("explorer.exe", System.IO.Directory.GetCurrentDirectory() + "\\Help.pdf");
+            }
+            catch (Exception ee) { }
+        }
+    }
+}
